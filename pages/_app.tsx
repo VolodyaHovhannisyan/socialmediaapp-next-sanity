f6906f6bd4bcac2a1b462e61cd670af140ea0707
VolodyaHovhannisyan <volodyahovhannisyan000@gmail.com>
2022-07-19 17:08:22 +0400

    Added UI and backend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import Navbar from '../src/components/Navbar'
+import Sidebar from '../src/components/Sidebar'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if (isSSR) return null
+
+
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`} >
+      <div>
+        <Navbar />
+        <div className="flex gap-6 md:gap-20">
+          <div className='h-[92] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex- 1'>
+            <Component {...pageProps} />
+          </div>
+
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default MyApp
